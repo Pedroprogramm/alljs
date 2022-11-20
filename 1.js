cd82ebcb2237023637d03d83a445810e97942426
stas <stas0rar@gmail.com>
2022-11-20 16:47:56 +0100

    8 COMM

--- 1.js
+++ 1.js
@@ -136,3 +136,16 @@ console.log(newSpreatObject);
 console.log(oldSpreatObject);
 // не копирует обьект в обьекте
 
+function createCounter() {
+    let counter = 0;
+
+    const myFunction = function() {
+        counter = counter + 1;
+        return counter;
+    }
+return myFunction;
+}
+
+const increment = createCounter();
+const c1 = increment();
+console.log(increment , c1); 
